5f75636d1a92a2aba17b47691e29dee5d7e3c7f4
Jamil Campeão <jamilcampeao@gmail.com>
2025-09-21 12:38:21 -0300

    fix: ajustes controlador cartão

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -5,7 +5,7 @@ export const insertCard = async (req, res) => {
   const userId = req.user.id;
   const { name, type, limit, closingDay, dueDate, accountId } = req.body;
 
-  if (!name || !type || !limit || !closingDay || !dueDate || !accountId) {
+  if (!name || !type || !closingDay || !dueDate || !accountId) {
     return res
       .status(400)
       .json({ message: "Todos os campos são obrigatórios." });
@@ -13,11 +13,13 @@ export const insertCard = async (req, res) => {
 
   try {
     const account = await prisma.account.findFirst({
-        where: { id: accountId, userId: userId }
+      where: { id: accountId, userId: userId },
     });
 
     if (!account) {
-        return res.status(404).json({ message: "Conta não encontrada ou não pertence ao usuário." });
+      return res
+        .status(404)
+        .json({ message: "Conta não encontrada ou não pertence ao usuário." });
     }
 
     const newCard = await prisma.card.create({
@@ -29,6 +31,18 @@ export const insertCard = async (req, res) => {
         dueDate: parseInt(dueDate),
         accountId,
         userId,
+      },
+      include: {
+        account: {
+          select: {
+            name: true,
+            bank: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
       },
     });
 
@@ -53,11 +67,11 @@ export const getAllCards = async (req, res) => {
             name: true,
             bank: {
               select: {
-                name: true
-              }
-            }
-          }
-        }
+                name: true,
+              },
+            },
+          },
+        },
       },
       orderBy: {
         name: "asc",
@@ -72,122 +86,153 @@ export const getAllCards = async (req, res) => {
 };
 
 export const getCardById = async (req, res) => {
-    const userId = req.user.id;
-    const cardId = parseInt(req.params.id);
+  const userId = req.user.id;
+  const cardId = parseInt(req.params.id);
 
-    try {
-        const card = await prisma.card.findFirst({
-            where: {
-                id: cardId,
-                userId: userId,
-            },
-            include: {
-              account: true
-            }
-        });
+  try {
+    const card = await prisma.card.findFirst({
+      where: {
+        id: cardId,
+        userId: userId,
+      },
+      include: {
+        account: true,
+      },
+    });
 
-        if (!card) {
-            return res.status(404).json({ message: "Cartão não encontrado." });
-        }
-
-        return res.status(200).json(card);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Erro ao buscar o cartão." });
+    if (!card) {
+      return res.status(404).json({ message: "Cartão não encontrado." });
     }
+
+    return res.status(200).json(card);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Erro ao buscar o cartão." });
+  }
 };
 
-
 export const updateCard = async (req, res) => {
-    const userId = req.user.id;
-    const cardId = parseInt(req.params.id);
-    const { name, type, limit, closingDay, dueDate, accountId } = req.body;
+  const userId = req.user.id;
+  const cardId = parseInt(req.params.id);
+  const { name, type, limit, closingDay, dueDate, accountId } = req.body;
 
-    if (!name || !type || !limit || !closingDay || !dueDate || !accountId) {
-        return res
-          .status(400)
-          .json({ message: "Todos os campos são obrigatórios." });
-      }
+  if (!name || !type || !limit || !closingDay || !dueDate || !accountId) {
+    return res
+      .status(400)
+      .json({ message: "Todos os campos são obrigatórios." });
+  }
 
-    try {
-        const card = await prisma.card.findFirst({
-            where: { id: cardId, userId: userId }
-        });
+  try {
+    const card = await prisma.card.findFirst({
+      where: { id: cardId, userId: userId },
+    });
 
-        if (!card) {
-            return res.status(404).json({ message: "Cartão não encontrado." });
-        }
-
-        const updatedCard = await prisma.card.update({
-            where: { id: cardId },
-            data: {
-                name,
-                type: CardType[type],
-                limit: parseFloat(limit),
-                closingDay: parseInt(closingDay),
-                dueDate: parseInt(dueDate),
-                accountId
-            },
-        });
-
-        return res.status(200).json(updatedCard);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Erro ao atualizar o cartão." });
+    if (!card) {
+      return res.status(404).json({ message: "Cartão não encontrado." });
     }
+
+    const updatedCard = await prisma.card.update({
+      where: { id: cardId },
+      data: {
+        name,
+        type: CardType[type],
+        limit: parseFloat(limit),
+        closingDay: parseInt(closingDay),
+        dueDate: parseInt(dueDate),
+        accountId,
+      },
+      include: {
+        account: {
+          select: {
+            name: true,
+            bank: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return res.status(200).json(updatedCard);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Erro ao atualizar o cartão." });
+  }
 };
 
 export const deleteCard = async (req, res) => {
-    const userId = req.user.id;
-    const cardId = parseInt(req.params.id);
+  const userId = req.user.id;
+  const cardId = parseInt(req.params.id);
 
-    try {
-        const card = await prisma.card.findFirst({
-            where: { id: cardId, userId: userId }
-        });
+  try {
+    const card = await prisma.card.findFirst({
+      where: { id: cardId, userId: userId },
+    });
 
-        if (!card) {
-            return res.status(404).json({ message: "Cartão não encontrado." });
-        }
-
-        await prisma.card.delete({
-            where: { id: cardId },
-        });
-
-        return res.status(200).json({ message: "Cartão excluído com sucesso." });
-    } catch (error) {
-        console.error(error);
-        // Tratar erro de restrição de chave estrangeira caso o cartão possua transações
-        if (error.code === 'P2003') {
-            return res.status(400).json({ message: "Não é possível excluir o cartão pois ele possui transações vinculadas." });
-        }
-        return res.status(500).json({ message: "Erro ao excluir o cartão." });
+    if (!card) {
+      return res.status(404).json({ message: "Cartão não encontrado." });
     }
+
+    await prisma.card.delete({
+      where: { id: cardId },
+    });
+
+    return res.status(200).json({ message: "Cartão excluído com sucesso." });
+  } catch (error) {
+    console.error(error);
+    // Trato erro de restrição de chave estrangeira caso o cartão possua transações
+    if (error.code === "P2003") {
+      return res.status(400).json({
+        message:
+          "Não é possível excluir o cartão pois ele possui transações vinculadas.",
+      });
+    }
+    return res.status(500).json({ message: "Erro ao excluir o cartão." });
+  }
 };
 
 export const inactiveCard = async (req, res) => {
-    const userId = req.user.id;
-    const cardId = parseInt(req.params.id);
+  const userId = req.user.id;
+  const cardId = parseInt(req.params.id);
 
-    try {
-        const card = await prisma.card.findFirst({
-            where: { id: cardId, userId: userId },
-        });
+  try {
+    const card = await prisma.card.findFirst({
+      where: {
+        id: cardId,
+        userId: userId,
+      },
+    });
 
-        if (!card) {
-            return res.status(404).json({ message: "Cartão não encontrado." });
-        }
-
-        const updatedCard = await prisma.card.update({
-            where: { id: cardId },
-            data: {
-                active: !card.active,
-            },
-        });
-
-        return res.status(200).json(updatedCard);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Erro ao alterar status do cartão." });
+    if (!card) {
+      return res.status(404).json({ message: "Cartão não encontrado." });
     }
+
+    const updatedCard = await prisma.card.update({
+      where: { id: cardId },
+      data: {
+        active: !card.active,
+      },
+      include: {
+        account: {
+          select: {
+            name: true,
+            bank: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return res.status(200).json(updatedCard);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Erro ao alterar status do cartão." });
+  }
 };
